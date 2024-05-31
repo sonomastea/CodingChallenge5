2ee21da6c52b74350c83948babfb14c4ea055eda
sonomastea <sbstea@usf.edu>
2024-05-30 23:34:59 -0400

    Instantiate Manager objects and calculate annual salaries with binuses.

--- cc_5.js
+++ cc_5.js
@@ -24,14 +24,22 @@ class Manager extends Employee{
         this.department=department;
 }
 }
+
 calculateAnnualSalary() ; {
     const annualSalary=super.calculateAnnualSalary();
     const bonus = annualSalary*0.15;
     const totalAnnualSalary=  (annualSalary+bonus); 
     console.log(`Bonus: ${bonus}`);
     console.log(`The total annual salary with bonus is ${totalAnnualSalary}`);
-
 }
+const manager1 = new Manager(`Bob Ross`, 6600, `Digital Marketing`);
+const manager2 = new Manager (`Debbie Little`, 7205, `Finance`);
+
+console.log(`{manager1.name}'s Annual Salary: ${manager1.calculateAnnualSalary}`);
+console.log(`${manager2.name}'s Annual Salary: ${manager2.calculateAnnualSalary});
+
+
+
 
 
 
