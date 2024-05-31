468fe6cc35cd103c42af8ca45f648cc6f5d852c4
sonomastea <sbstea@usf.edu>
2024-05-30 22:31:05 -0400

    test

---
+++ cc_5.js
@@ -0,0 +1,13 @@
+//U05721483
+//Employee and Manager Calculation Challenge
+//Step 1
+class Employee {
+    const(name, salary) {
+        this.name=name 
+        this.salary=salary
+        console.log(`Employee name: ${this.name}), Monthly Salary:$
+        {this.salary}`);
+
+    }
+  
+}
