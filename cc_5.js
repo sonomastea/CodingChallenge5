1dfe287009e3df4f3ac099794ebe691d943d4b95
sonomastea <sbstea@usf.edu>
2024-05-30 23:06:24 -0400

    Create a manager subclass with bonus   calculation

--- cc_5.js
+++ cc_5.js
@@ -12,7 +12,28 @@ class Employee {
         this.salary*12;
 
     }
-    
-}
        
   
+
+}
+
+//Step 2
+class Manager extends Employee{
+    constructor ( name,salary,department){
+        super(name,salary);
+        this.department=department;
+}
+}
+calculateAnnualSalary() ; {
+    const annualSalary=super.calculateAnnualSalary();
+    const bonus = annualSalary*0.15;
+    const totalAnnualSalary=  (annualSalary+bonus); 
+    console.log(`Bonus: ${bonus}`);
+    console.log(`The total annual salary with bonus is ${totalAnnualSalary}`);
+
+}
+
+
+
+
+
